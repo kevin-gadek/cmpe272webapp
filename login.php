370c722d56ef317d23e2e6b2627f4624fb957a4f
kevin-gadek <kgadek@sbcglobal.net>
2017-11-25 20:30:02 -0800

    init template

---
+++ login.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Spartan Shop</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/shop-login.css" rel="stylesheet">
+
+  </head>
+
+  <body style="height:100%">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tracker.php">Tracking</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/register.php">Register</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="/login.php">Log In
+				<span class="sr-only">(current)</span>
+			  </a>
+			  
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+    </form>
+  </div>
+</div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark" style="position:absolute; bottom:0; width:100%">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Spartan Shop 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
