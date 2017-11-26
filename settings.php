5675d1201f95d77cc872b06cec7facdf7c7f7f6c
Huy Vo <huyvanvo94@gmail.com>
2017-11-26 12:43:54 -0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 
 /*Created by huyvo on 11/26/2017*/
 
-/* Change this for local/remove DB access */
+/* Change this for local/remote DB access */
 $settings = [
     'host' => '127.0.0.1',
     'port' => '3306',
