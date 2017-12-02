9ba2f7f33fdeb7b3e769f043f5e89aaa93b00bc9
玄     宫 <gen@Oliver-Gongs-MacBook-Pro.local>
2017-12-02 13:04:20 -0800

    tracking

--- schema.sql
+++ schema.sql
@@ -20,6 +20,7 @@ CREATE TABLE Tracking(
 	product_id INT,
 	date_created INT,
 	company_id INT,
+	review INT,
 	PRIMARY KEY (_id),
 	FOREIGN KEY (user_id) REFERENCES Users(_id)
 
@@ -45,7 +46,12 @@ INSERT INTO Tracking (user_id, product_id, date_created, company_id) VALUES (2, 
 
 INSERT INTO Tracking (user_id, product_id, date_created, company_id) VALUES (2, 3, NULL, 1);
 INSERT INTO Tracking (user_id, product_id, date_created, company_id) VALUES (2, 2, NULL, 1);
-INSERT INTO Tracking (user_id, product_id, date_created, company_id) VALUES (2, 2, NULL, 1);
+
+INSERT INTO Tracking (user_id, product_id, date_created, company_id,review) VALUES (2, 2, NULL, 1,1);
+INSERT INTO Tracking (user_id, product_id, date_created, company_id,review) VALUES (2, 1, NULL, 1,3);
+INSERT INTO Tracking (user_id, product_id, date_created, company_id,review) VALUES (2, 1, NULL, 1,2);
+INSERT INTO Tracking (user_id, product_id, date_created, company_id,review) VALUES (2, 3, NULL, 1,5);
+INSERT INTO Tracking (user_id, product_id, date_created, company_id,review) VALUES (2, 2, NULL, 1,4);
 -- for top 5 most visited total for whole market
 select product_id, count(_id) as instance
 from tracking
@@ -68,3 +74,10 @@ from Tracking
 where user_id=1
 group by(company_id)) as g
 order by recent_date desc;
+
+-- top 5 best review
+select product_id,sum(review)/count(_id) as avg_review
+from tracking
+group by product_id
+order by avg_review desc
+limit 5;
