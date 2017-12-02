a057c4252897b8e61c70a7391f0c6c4506b6d8c5
kevin-gadek <kgadek@sbcglobal.net>
2017-12-02 13:07:33 -0800

    basic tracker ui

---
+++ public/tracker.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Spartan Shop</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/shop-tracker.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tracker.php">Tracking</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/register.php">Register</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="/login">Log In</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        
+    <div class="top-five">
+        <br>
+        <h2 class="m-0 text-center text-black">Most Visited Marketplace Products</h2>
+        <br>
+        <!-- top five list here -->
+        <ul>
+        <li>Product 1</li>
+         <li>Product 2</li>
+         <li>Product 3</li>
+        </ul>
+    </div>
+    
+    <div class="top-five-by-company">
+        <div class="company">
+            <h3>Tutoring</h3>
+            <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>    
+            </ul>
+        </div>    
+        <div class="company" >
+            <h3>Gemstones</h3>
+            <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>    
+            </ul>
+        </div> 
+         <div class="company" >
+            <h3>Interior Design</h3>
+            <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>    
+            </ul>
+        </div>   
+        <div class="company">
+            <h3>Chinese Resturant</h3>
+            <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>    
+            </ul>
+        </div>   
+        <div class="company">
+            <h3>Construction</h3>
+            <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>    
+            </ul>
+        </div>   
+    </div> <!-- top-five-company -->
+
+    <div class="best-reviewed">
+        <h2 class="m-0 text-center text-black">Best Reviewed Marketplace Products</h2>
+        <br>
+                <!-- top five list here -->
+        <ul>
+        <li>Product 1</li>
+         <li>Product 2</li>
+         <li>Product 3</li>
+        </ul>
+    </div> <!-- best-reviewed -->
+      
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark" style="position:absolute; bottom:0; width:100%">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Spartan Shop 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
